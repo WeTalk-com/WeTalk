68e97f4aa824cef63fb8ed074ae1938dc739a63c
Gabriel Roche <gabirocher@icloud.com>
2026-06-24 17:32:38 +0200

    C'est un MODULE

--- apps/user-service/src/middleware/auth.ts
+++ apps/user-service/src/middleware/auth.ts
@@ -5,8 +5,9 @@ import { logger } from "../utils/logger.js";
 import type { UserRole } from "../models/user.js";
 
 declare global {
-  // eslint-disable-next-line @typescript-eslint/no-namespace
-  namespace Express {
+  
+  // eslint-disable-next-line
+  module Express {
     interface Request {
       user?: JwtPayload;
     }
