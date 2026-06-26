2a10e921cee3950800f6278727107c3a4b0e3ef8
Timothé <timothe.fardella@viacesi.fr>
2026-06-26 05:46:11 +0200

    fix : CI faillure

--- vitest.config.ts
+++ vitest.config.ts
@@ -3,6 +3,12 @@ import { defineConfig } from "vitest/config";
 export default defineConfig({
   test: {
     include: ["apps/*/src/**/*.test.ts"],
+    ...(process.env.CI
+      ? {
+          reporters: ["default", "junit"],
+          outputFile: { junit: "junit.xml" },
+        }
+      : {}),
     coverage: {
       provider: "v8",
       include: ["apps/*/src/**"],
